7e4867579b09134a6b310326ac8d71c68dbcea35
= <=>
2023-10-13 19:23:18 +0300

    Change the Logo

--- resources/js/pages/Navbar.vue
+++ resources/js/pages/Navbar.vue
@@ -2,7 +2,7 @@
     <div className="flex items-center w-screen h-[10vh] bg-white shrink-0">
         <div className="w-[65vw]">
             <div
-                className="w-[8vh] h-[8vh] ml-[8vw] my-auto bg-[url('https://s3-alpha-sig.figma.com/img/394c/ab78/d800e629a0407d3c746f3c59a15694f1?Expires=1698019200&Signature=mM1YLtYo2qOuSNKDAv3vCrYY1639IfoVACZ8V2Y6lkQ0aOffKLteqb6q2-XfHbZlufycVwSg~W4O910MRXMjWOTbcbD6GY5x~pFM8Ulx3QG2Ui1FaPrGULJiZTsuH~hDUc5rkzAdhqv~GhjFd18sxtXfEq8qt9XnP-p1-ugbHWKWAGWvWvOvQY9hotRk4V56ohkJnPIDEoD1tMPt6E-c~pDRwrEwVaIS0rlPsXxGQvVJGSsmL5MbrWsZ3mxQub1Fy~TxY1nS2KEcgwnNB~GYiiIkuxVKQmJAiLGwk5Um9nUI9BQvEJI011bO5I9F84ZmhZGL47YI8PP7Ht7sn5b3Nw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4')] bg-cover bg-no-repeat"
+                className="w-[20vh] h-[8vh] ml-[8vw] my-auto bg-[url('assets/img/Logo.png')] bg-cover bg-no-repeat"
             ></div>
         </div>
         <div className="flex items-center w-[35vw]">
