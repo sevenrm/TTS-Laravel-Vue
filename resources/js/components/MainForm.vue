c12e290321800c84a623e86afa00eaa14eae4890
= <=>
2023-10-15 08:39:19 +0300

    Set the font-family to 'SF Pro Text

--- resources/js/components/MainForm.vue
+++ resources/js/components/MainForm.vue
@@ -1,10 +1,10 @@
 <template>
     <form
-        className="flex flex-col w-[44vw] h-[33vw] bg-white my-[5vh] py-[3%] px-[2%] shrink-0 rounded-md"
+        className="flex flex-col w-[60vw] h-[40vw] bg-white my-[5vh] py-[3%] px-[2%] shrink-0 rounded-md"
     >
         <label className="text-[14px] font-normal mb-[2%]">Language</label>
         <select
-            className=" w-full h-[12%] mb-[3%] border border-[##DDE1EB] rounded-lg pl-[3%] pr-[3%] text-[#23262A] text-[16px] font-medium leading-5"
+            className=" w-full h-[10%] mb-[3%] border border-[##DDE1EB] rounded-lg pl-[3%] pr-[3%] text-[#23262A] text-[16px] font-medium leading-5"
             placeholder="select language"
             @change="
                 (e) => {
@@ -22,7 +22,7 @@
         </select>
         <label className="text-[14px] font-normal mb-[2%]">Character</label>
         <select
-            className="w-full h-[12%] mb-[3%] border border-[##DDE1EB] rounded-lg px-[3%] text-[#23262A] text-[16px] font-medium leading-5 invalid:text-[#B2BAC6]"
+            className="w-full h-[10%] mb-[3%] border border-[##DDE1EB] rounded-lg px-[3%] text-[#23262A] text-[16px] font-medium leading-5 invalid:text-[#B2BAC6]"
             placeholder="select character"
             @change="
                 (e) => {
@@ -42,7 +42,7 @@
         </select>
         <label className="text-[14px] font-normal mb-[2%]">Text</label>
         <textarea
-            className="w-full h-[20%] border border-[##DDE1EB] rounded-lg px-[3%] py-[2%] text-[16px] font-medium leading-5 mb-[3%]"
+            className="w-full h-[30%] border border-[##DDE1EB] rounded-lg px-[3%] py-[2%] text-[16px] font-medium leading-5 mb-[3%]"
             cols="3"
             placeholder="Enter text"
             @change="
